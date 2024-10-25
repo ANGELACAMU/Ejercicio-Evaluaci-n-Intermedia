d5e7b5d9c6ceb697e504dbbb3103cac31be8b28a
anyo333@gmail.com <angelacm@DESKTOP-JHMM9DJ.localdomain>
2024-10-25 09:42:11 +0200

    adding const and first function to js

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 'use strict';
 
+const input = document.querySelector(".js-input");
+const button = document.querySelector(".js-button");
+
+const pista = document.querySelector(".js-pista");
+
+function getRandomNumber(max) {
+ return Math.ceil(Math.random() * max);
+}
 
 
